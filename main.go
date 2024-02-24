338940dd146c09b7a5ea565c043afc9612e3bfae
adeo <zadewale12@gmail.com>
2024-02-24 20:25:48 +0000

    Changed routing for CSS files.

--- main.go
+++ main.go
@@ -30,7 +30,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	http.Handle("/css/", http.FileServer(http.Dir("./public")))
+	http.Handle("/public/css/", http.FileServer(http.Dir("./")))
 	http.HandleFunc("/", handler)
 	http.ListenAndServe("", nil)
 }
