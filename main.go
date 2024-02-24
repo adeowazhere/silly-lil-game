938086d1c6a3d871083234fb55a2d9630f9059a6
adeo <zadewale12@gmail.com>
2024-02-24 19:58:35 +0000

    Initialised Go HTML package

--- main.go
+++ main.go
@@ -1,7 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+func startPage(w http.ResponseWriter, r *http.Request) {
+	var fileName = "start.html"
+	page, err := template.ParseFiles(fileName)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
+	}
+
+	err = page.ExecuteTemplate(w, fileName, nil)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
+	}
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/start":
+		startPage(w, r)
+	default:
+		var fileName = "index.html"
+		page, err := template.ParseFiles(fileName)
+		if err != nil {
+			fmt.Println("Error: ", err)
+		}
+
+		err = page.ExecuteTemplate(w, fileName, nil)
+		if err != nil {
+			fmt.Println("Error: ", err)
+		}
+	}
+}
 
 func main() {
-	fmt.Println("The systems do seem to be online.")
+	http.Handle("/css/", http.FileServer(http.Dir("./public")))
+	http.HandleFunc("/", handler)
+	http.ListenAndServe("", nil)
 }
